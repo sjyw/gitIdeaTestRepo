4df60cd4b1072ee401191a275132abd2ef7a3e2b
夜流星 <857176188@qq.com>
2020-02-27 23:24:04 +0800

    修改Hello文件,添加了一个add()方法

--- ideaGitTest1/src/com/ke/Hello.java
+++ ideaGitTest1/src/com/ke/Hello.java
@@ -4,5 +4,16 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Git Test!");
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x + "+" + y + "=" + add(x, y));
+    }
+
+    /**
+     * 加法
+     */
+    private static int add(int x, int y) {
+        return x + y;
     }
 }
